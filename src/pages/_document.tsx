4fb6dfe3a1990b74f1dd1c443a618f5c587e839f
ivopereira-jr <ivopereira.jr12@gmail.com>
2021-08-11 18:47:43 -0300

    :sparkles: ad google

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,6 +12,12 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"
             rel="stylesheet"
           />
+
+          <script
+            data-ad-client="ca-pub-8881491007658106"
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+          />
         </Head>
         <body>
           <Main />
